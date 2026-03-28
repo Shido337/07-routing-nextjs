9d00f0ac438393bc3a84c6efac8fce3b465e0c9a
Русальчук Андрей <anrusalchuk@gmail.com>
2026-03-28 16:52:50 +0200

    .

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -2,15 +2,8 @@ import css from './layout.module.css';
 
 export default function NotesLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
-  return (
-    <div className={css.main}>
-      {children}
-      {modal}
-    </div>
-  );
+  return <div className={css.main}>{children}</div>;
 }
